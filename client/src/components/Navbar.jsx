3e800c372436148620e4b9c74579b186ed724b10
Eric Graham <8397596+wiltzie75@users.noreply.github.com>
2025-07-14 16:55:55 -0400

    created api.js and Navbar.jsx files

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { AppBar, Toolbar, Button, IconButton, Avatar } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+
+const Navbbar = () => {
+    return (  );
+}
+ 
+export default Navbbar;
