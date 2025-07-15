914e509007c2a5b5a04e9c97c79c98d98e5f558a
Eric Graham <8397596+wiltzie75@users.noreply.github.com>
2025-07-15 11:29:29 -0400

    installed react-router-dom and updated navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { Link, Navigate, NavLink } from 'react-router-dom';
+
 const Navbar = () => {
+    const [departments, setDepartments] = useState([]);
+    const [faculty, setFaculty] = useState([]);
+
+    
     return (  );
 }
  
